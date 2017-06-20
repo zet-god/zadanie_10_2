4d1e62e84eedb1020b9fad4899ff27a508d27034
Zuzanna Godek <[omitted]>
2017-06-20 18:32:01 +0200

    stworzono pliki .index.html i scrpits.js

---
+++ scripts.js
@@ -0,0 +1,31 @@
+//alert('hello');
+
+var span = $("span");
+
+// span.each(function () {
+// 	span.css('color, 'green;);
+// });
+
+// $(function() {
+// 	$('span').css('color', 'yellow');
+// });
+
+
+span.each(function(index, element) {
+console.log(index);
+	if(index % 2 == 0) {
+		$(element).css('color', 'yellow');
+	};
+	//$("span:even").css('color', 'yellow');
+});
+
+var p = $("p");
+
+p.each(function (index, element) {
+	var button = '<button class="btn" data-tmp="' + index + '">Click</button>'
+	$(element).append(button)
+});
+
+$("button").click(function() {
+	alert($(this).attr("data-tmp"));
+});
